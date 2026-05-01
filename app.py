436723e024e04aa412e3d545de049cfa02116739
sureshk248 <sureshkoneti.248@gmail.com>
2026-05-01 16:31:25 +0530

    updated

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask,request,redirect,render_template
+from flask import Flask,request,redirect,render_template,session
 import sqlite3
+import random
 
 app = Flask(__name__)
+app.secret_key = "secret123"
 
 def init_db():
     conn = sqlite3.connect("app.db")
     cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        mobile TEXT UNIQUE,
+        password TEXT
+    )
+    """)
 
     cursor.execute("""
      CREATE TABLE IF NOT EXISTS employees(
@@ -21,8 +31,72 @@ def init_db():
 
 init_db()
 
+# ---------------- SIGNUP ----------------
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        mobile = request.form["mobile"]
+        password = request.form["password"]
+
+        session["temp_mobile"] = mobile
+        session["temp_password"] = password
+
+        conn = sqlite3.connect("app.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "INSERT INTO users (mobile, password) VALUES (?, ?)",
+            (mobile, password)
+        )
+
+        conn.commit()
+        conn.close()
+
+        return redirect("/login")
+
+    return render_template("signup.html")
+
+# ---------------- LOGIN ----------------
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        mobile = request.form["mobile"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("app.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE mobile=? LIMIT 1",
+            (mobile,)
+        )
+
+        user = cursor.fetchone()
+        conn.close()
+
+        if user:
+            session["user"] = mobile
+            return redirect("/first")
+        else:
+            return "Invalid credentials"
+
+    return render_template("login.html")
+
+
+# ---------------- LOGOUT ----------------
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect("/login")
+
+
+# ---------------- HOME ----------------#
 @app.route("/")
-def form():
+def home():
+    return redirect("/login")
+
+@app.route("/first")
+def first():
     return render_template("form.html")
 
 @app.route("/add",methods=["POST"])
@@ -56,4 +130,4 @@ def list():
     return render_template("list.html",tab1 = rows)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
